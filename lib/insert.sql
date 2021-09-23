a8ed80cc715b5dd5601acf020531a79aa858a581
chaim <chaimyisroel@gmail.com>
2021-09-23 11:01:26 -0500

    commit tree 95a7743de2723d89bf11db2e2307ccaa8ffdd251

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, age, alive) VALUES ('Mr. Chocolate', 'M', null, null);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Rowdy', 'F', 10, 1);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Tabitha', 'F', 6, 1);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Sergeant Brown', 'F', null, null);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Melissa', 'M', 13, 1);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Grinch', 'F', 2, 1);
+INSERT INTO bears (name, sex, age, alive) VALUES ('Wendy', 'M', 6, 1);
+INSERT INTO bears (name, sex, age, alive) VALUES (null, null, null, null);
